e3b21b799562815b569e3fcd9427405612d49187
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2020-04-18 00:21:14 -0700

    Updates

---
+++ precache-manifest.3e1296655749d84e6f063b8c28654c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a295c701118bdfc29e60696ec948ae",
+    "url": "/drawing-app/index.html"
+  },
+  {
+    "revision": "fda312bdc40fd283af3f",
+    "url": "/drawing-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d72af49076e19af3b424",
+    "url": "/drawing-app/static/js/2.8aedab6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/drawing-app/static/js/2.8aedab6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda312bdc40fd283af3f",
+    "url": "/drawing-app/static/js/main.3b2219b4.chunk.js"
+  },
+  {
+    "revision": "288c582bff9ef1cb183e",
+    "url": "/drawing-app/static/js/runtime-main.8e7e8016.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/drawing-app/precache-manifest.1048fe2ecc1d3a0bba5bf7ef69242d17.js"
+  "/drawing-app/precache-manifest.3e1296655749d84e6f063b8c28654c93.js"
 );
 
 self.addEventListener('message', (event) => {
