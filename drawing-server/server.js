040265b3fed10fad0f6adb2fa8aa89c3958f4599
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2020-05-01 20:20:12 -0700

    add: socket.io test files

---
+++ drawing-server/server.js
@@ -0,0 +1,32 @@
+const express = require('express'), 
+    app = express(),
+    http = require('http'),
+    socketIo = require('socket.io');
+
+// start webserver on port 8080
+const server =  http.createServer(app);
+const io = socketIo.listen(server);
+server.listen(8080);
+// add directory with our static files
+app.use(express.static(__dirname + '/public'));
+console.log("Server running on 127.0.0.1:8080");
+
+// array of all lines drawn
+let line_history = [];
+
+// event-handler for new incoming connections
+io.on('connection', function (socket) {
+
+   // first send the history to the new client
+   for (let i in line_history) {
+      socket.emit('draw_line', { line: line_history[i] } );
+   }
+
+   // add handler for message type "draw_line".
+   socket.on('draw_line', function (data) {
+      // add received line to history 
+      line_history.push(data.line);
+      // send line to all clients
+      io.emit('draw_line', { line: data.line });
+   });
+});
