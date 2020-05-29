d87851ef6c06569eaafff824cf996523e72bf7ad
Theo <24834291+HelloTakako@users.noreply.github.com>
2020-05-28 20:59:00 -0700

    test for material UI library

--- drawing-client/src/components/pages/RoomPage.js
+++ drawing-client/src/components/pages/RoomPage.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import { useEffect } from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Grid';
 import Grid from '@material-ui/core/Grid';
 
 import Canvas from '../Canvas';
 import ChatBox from '../ChatBox';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(5)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function RoomPage (props) {
   useEffect(() => {
     document.title = `Room`;
   });
 
+  const classes = useStyles();
+
   return (
-    <Grid
-      container
-      direction="row"
-      justify="flex-start"
-      alignItems="flex-start"
-    >
-            <Canvas />
-            <ChatBox />
-    </Grid>
+    <div className={classes.root}>
+      <Container maxWidth="sm">
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+                  <Grid item xs={6}>
+                    <Canvas />
+                  </Grid>
+                  <Grid item xs={6}>
+                      <ChatBox />
+                  </Grid>              
+        </Grid>
+      </Container>
+    </div>
   );
 }
 
